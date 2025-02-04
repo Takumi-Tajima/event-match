81ef6e937ae893bbc61a107b734621c2807a5d62
taji <taji@sonicgarden.jp>
2025-02-04 21:51:26 +0900

    認証系のメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :authenticate_user!
+  helper_method :current_user, :user_signed_in?
+
+  def authenticate_user!
+    return if current_user
+
+    redirect_to root_path, alert: 'ログインしてください'
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def current_user
+    return unless (user_id = session[:user_id])
+
+    @current_user ||= User.find_by(id: user_id)
+  end
 end
