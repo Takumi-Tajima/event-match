2fcfa8a6d6448bf2e1afabe12efa3380df823a1b
taji <taji@sonicgarden.jp>
2025-02-04 22:28:45 +0900

    トップページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  root 'home#index'
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   post 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
