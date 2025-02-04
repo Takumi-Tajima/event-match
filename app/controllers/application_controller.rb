3df46b5f82b7aadeecb1cd80e8bda312f4531511
taji <taji@sonicgarden.jp>
2025-02-04 22:29:20 +0900

    ログインおよびログアウトメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,13 @@ class ApplicationController < ActionController::Base
 
     @current_user ||= User.find_by(id: user_id)
   end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
