e3fa6c32a89a6f51a4180b78ebbeef12a3447a0f
taji <taji@sonicgarden.jp>
2025-02-07 01:38:44 +0900

    checkedを更新する

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,7 @@
 class CalendarsController < ApplicationController
   def create
     current_user.create_event(params[:summary], params[:start_date], params[:end_date], params[:url])
+    Event.find(params[:id]).update!(checked: true)
+    redirect_to events_path
   end
 end
