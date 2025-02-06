b800c6982d4a4a65cec2d8109eb9e91ae19b8146
taji <taji@sonicgarden.jp>
2025-02-06 18:34:49 +0900

    ログイン リファクタ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[create]
 
   def create
-    user = find_or_create_from_auth_hash(auth_hash)
+    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     log_in user if user
     redirect_to root_path
   end
@@ -10,18 +10,5 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to root_path
-  end
-
-  private
-
-  def auth_hash
-    request.env['omniauth.auth']
-  end
-
-  def find_or_create_from_auth_hash(auth_hash)
-    email = auth_hash['info']['email']
-    User.find_or_create_by(email: email) do |user|
-      user.update(email: email)
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth_hash(auth_hash)
+    name = auth_hash['info']['name']
+    email = auth_hash['info']['email']
+    token = auth_hash['credentials']['token']
+    refresh_token = auth_hash['credentials']['refresh_token']
+
+    User.find_or_create_by!(email: email) do |user|
+      user.name = name
+      user.token = token
+      user.refresh_token = refresh_token
+    end
+  end
 end
