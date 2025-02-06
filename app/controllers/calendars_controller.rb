33607fc086622318bd11536ad6d61a2f43372a94
taji <taji@sonicgarden.jp>
2025-02-07 01:00:18 +0900

    create_eventメソッドにURLパラメータを追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,5 @@
 class CalendarsController < ApplicationController
   def create
-    current_user.create_event(params[:summary], params[:start_date], params[:end_date])
+    current_user.create_event(params[:summary], params[:start_date], params[:end_date], params[:url])
   end
 end
