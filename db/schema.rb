d1a72ba0f0960b13d84c05f65df94562ed275658
taji <taji@sonicgarden.jp>
2025-02-06 18:26:40 +0900

    tokenとrefresh_tokenカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_04_131824) do
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
+    t.string "token", default: "", null: false
+    t.string "refresh_token", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
