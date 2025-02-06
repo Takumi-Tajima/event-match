2678020213864a97b7631950551b34c27118304b
taji <taji@sonicgarden.jp>
2025-02-06 19:17:47 +0900

    eventモデル作成

---
+++ db/migrate/20250206101603_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[8.0]
+  def change
+    create_table :events do |t|
+      t.string :name, null: false
+      t.datetime :date, null: false
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_04_131824) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_06_101603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "date", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
